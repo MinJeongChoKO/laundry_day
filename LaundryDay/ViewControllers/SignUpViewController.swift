d48305b6ea94854b60334b1839c1c67f4c972085
MBP03 <mbp03@Admins-MacBook-Pro.local>
2018-04-14 17:06:10 +0900

    02.Storyboards

---
+++ LaundryDay/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SignUpViewController.swift
+//  LaundryDay
+//
+//  Created by MBP03 on 2018. 4. 14..
+//  Copyright © 2018년 MBP03. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func logInButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
